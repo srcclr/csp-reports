33efbd477bfe8272713d99e9a5d65206fdf08dc2
Arthur Zaharov <arthur.zaharov@flatstack.com>
2015-10-13 11:23:20 +0300

    Get rid of tests for now

--- spec/factories/domain.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :domain, class: "CspReports::Domain" do
-    name "Google"
-    url "google.com"
-  end
-end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,6 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 Dir["#{File.dirname(__FILE__)}/factories/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
