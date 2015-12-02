2e3593d410b991d66a91f79421cbc6b2ab5fa1de
ArthurZaharov <arthur.zaharov@flatstack.com>
2015-12-02 19:25:31 +0300

    Send notification mail after share domain with user

--- plugin.rb
+++ plugin.rb
@@ -8,6 +8,8 @@ register_asset("stylesheets/csp_reports/csp-reports.css.scss")
 
 after_initialize do
   require(File.expand_path("../app/models/user", __FILE__))
+  require(File.expand_path('../app/jobs/shared_domain', __FILE__))
+  require(File.expand_path('../app/mailers/shared_domain_mailer', __FILE__))
 end
 
 Discourse::Application.routes.append do
