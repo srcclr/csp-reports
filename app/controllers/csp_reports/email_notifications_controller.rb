f7c630f16b03d626e1775943a85a0ca948e289bf
RamilGilmanov <ramil.gilmanov@flatstack.com>
2015-12-04 13:29:21 +0300

    Force domain order

--- app/controllers/csp_reports/email_notifications_controller.rb
+++ app/controllers/csp_reports/email_notifications_controller.rb
@@ -3,10 +3,8 @@ module CspReports
     skip_before_action :check_xhr, :redirect_to_login_if_required, :verify_authenticity_token
 
     def update
-      notification = current_user.email_notifications.find_or_create_by(
-        csp_reports_domain_id: params[:domain_id],
-        notification_type: params[:notification_type]
-      )
+      notification = current_user.email_notifications.find_or_create_by(csp_reports_domain_id: params[:domain_id])
+      notification.update(notification_type: params[:notification_type])
 
       render json: notification
     end
