6d8006c9900719972557aed93ba06d6d187443bb
RamilGilmanov <ramil.gilmanov@flatstack.com>
2015-12-07 19:06:45 +0300

    Display reports for given timeframe, add table

--- app/mailers/csp_violations_report_mailer.rb
+++ app/mailers/csp_violations_report_mailer.rb
@@ -4,14 +4,32 @@ class CspViolationsReportMailer < ActionMailer::Base
   include Email::BuildEmailHelper
 
   def report(notification)
-    build_email(
-      notification.user.email,
+    email_opts = {
       template: "csp_violations_report",
-      notification_type: notification.notification_type.titleize,
-      username: notification.user.username,
-      domain_name: notification.domain.name,
-      domain_url: notification.domain.url,
-      report_url: "#{Discourse.base_url}/csp-reports/domains/#{notification.domain.id}"
+      html_override: html(notification),
+      notification_type: notification.notification_type.titleize
+    }
+
+    build_email(notification.user.email, email_opts)
+  end
+
+  private
+
+  def html(notification)
+    ActionView::Base.new("plugins/csp-reports/app/views").render(
+      template: 'email/csp_reports',
+      format: :html,
+      locals: {
+        reports: notification.domain.reports
+          .for_type(notification.notification_type)
+          .order(id: :desc)
+          .limit(10),
+        notification_type: notification.notification_type.titleize,
+        username: notification.user.username,
+        domain_name: notification.domain.name,
+        domain_url: notification.domain.url,
+        report_url: "#{Discourse.base_url}/csp-reports/domains/#{notification.domain.id}"
+      }
     )
   end
 end
